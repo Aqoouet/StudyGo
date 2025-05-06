753fe7a6fdee0d3db4d3a647cba8eac9ecee9367
Ryazanov Maxim <maximryazanoff@yandex.ru>
2025-05-06 18:15:51 +0300

    я не сохранял перед commit ранее теперь попробуем с сохранением.

--- server1/server1.go
+++ server1/server1.go
@@ -6,12 +6,16 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+func main() {
+
+	// регистрируем маршрутизатор handler который будет обрабатывать все запросы к сайту
+	http.HandleFunc("/", handler)
+
+	//стартуем сервер
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Println("Starting server at http://localhost:8000")
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+// функция для обработки запросов
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
